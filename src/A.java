2a1b65a14c81dd86b763d09710d748c9270ab6b1
2Hour <2904260265@qq.com>
2025-12-12 15:33:47 +0800

    1

--- src/A.java
+++ src/A.java
@@ -10,12 +10,8 @@ import java.util.Scanner;
 public abstract class A {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("A");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("vvv");
+        System.out.println("vvv");
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
